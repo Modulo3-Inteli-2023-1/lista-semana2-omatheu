4431fe79da29a249f5b9e8c3453d88a461e75c0a
omatheu <124204749+omatheu@users.noreply.github.com>
2023-08-18 20:57:34 -0300

    commit exercises

--- exercicio1.py
+++ exercicio1.py
@@ -5,7 +5,26 @@
 
 
 # Crie a função que será avaliada no exercício aqui
+def conta_palavras_unicas(frase):
+    
+    lista_palavras = frase.split()
 
+
+    palavras_unicas = set(lista_palavras)
+
+    quantidade = len(palavras_unicas)
+
+    return quantidade
+
+    # print(lista_palavras)
+
+    # aux = 0
+
+    # for i in lista_palavras:
+    #     print(i)
+    #     aux += 1
+
+    # return aux
 
 
 
@@ -13,14 +32,6 @@
 
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
+print(conta_palavras_unicas('string string a b'))
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -6,6 +6,34 @@
 
 # Crie a função que será avaliada no exercício aqui
 
+def is_anagram(palavra1, palavra2):
+    d = ''
+    # g = []
+    # for letras in palavra1:
+    #     print(letras)
+    #     d += letras
+    # for letra in palavra2:
+    #     print(letra)
+    #     g += letra
+
+    for letra in palavra1:
+        d += letra
+        print(d)
+
+    if d in palavra2:
+        return True
+    else:
+        print('Not is a anagram')
+        return False
+    # if d == g:
+    #     return True
+    # else:
+    #     print('Nada não')
+
+    # print(d)
+    # print(g)
+    # if len(palavra1) == len(palavra2):
+    #     return True
 
 
 
@@ -13,6 +41,10 @@
 
 
 # Teste a sua função aqui (caso ache necessário)
+a = 'amor'
+b = 'roma'
+
+print(is_anagram(a, b))
 
 
 
